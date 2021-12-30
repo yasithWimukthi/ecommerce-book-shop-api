63a70f8b0b013809d605b674a168a553ef96b49e
yasith wimukthi <it19966922@my.sliit.lk>
2021-12-30 15:41:28 +0530

    create user routes

--- routes/user.js
+++ routes/user.js
@@ -3,17 +3,13 @@ const router = express.Router();
 
 
 const { userById, read, update, purchaseHistory } = require('../controllers/user');
+const {requireSignin, isAuth} = require("../controllers/auth");
 
-// router.get('/secret', requireSignin, (req, res) => {
-//     res.json({
-//         user: 'got here yay'
-//     });
-// });
 
-// router.get('/user/:userId', requireSignin, isAuth, read);
-// router.put('/user/:userId', requireSignin, isAuth, update);
-// router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
-//
+router.get('/user/:userId', requireSignin, isAuth, read);
+router.put('/user/:userId', requireSignin, isAuth, update);
+router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
+
 router.param('userId', userById);
 
 module.exports = router;
